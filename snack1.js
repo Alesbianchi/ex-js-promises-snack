3f704b9677d4b03cd96c6394867d32caac3b8a16
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-04-30 10:34:04 +0200

    snack1 bonus

--- snack1.js
+++ snack1.js
@@ -1,22 +1,38 @@
 //Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
 
-function getPostTitle(id) {
+//Bonus: Ottieni l'intero post con l'autore
+// Crea una funzione getPost(id) che recupera l'intero post. Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
+
+function getPost(id) {
     return fetch(`https://dummyjson.com/posts/${id}`)
         .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
+            if (!response.ok) {
                 return Promise.reject(`Post non trovato`)
+
             }
+            return response.json();
+
         })
-        .then(data => data.title);
+        .then(post => {
+            return fetch(`https://dummyjson.com/users/${post.userId}`)
+                .then(response => {
+                    if (!response.ok) {
+                        return Promise.reject('Utente non trovato');
+                    }
+                    return response.json();
+                })
+                .then(user => {
+                    post.user = user;
+                    return post;
+                });
+        });
 }
 
-getPostTitle(1)
-    .then(title => {
-        console.log("Titolo del post:", title);
+getPost(1)
+    .then(post => {
+        console.log("Post completo:", post);
 
     })
     .catch(error => {
-        console.error("errore:", error.message);
+        console.error("errore:", error);
     });
